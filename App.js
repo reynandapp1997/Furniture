7bcc1f4f205e644098f85e9ae5f5ae629b761e79
Reynanda Putra Pratama <reynandapp1997@gmail.com>
2019-02-24 20:04:24 +0700

    add react-navigation

--- App.js
+++ App.js
@@ -7,44 +7,122 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, {
+  Component
+} from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { createAppContainer, createSwitchNavigator, createMaterialTopTabNavigator, createStackNavigator } from 'react-navigation';
+import IconFa from 'react-native-vector-icons/FontAwesome';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+import reducers from './src/redux/reducers';
+import FurnitureScreen from './src/screens/FurnitureScreen';
+import UserScreen from './src/screens/UserScreen';
+import UserAddScreen from './src/screens/UserAddScreen';
+import UserUpdateScreen from './src/screens/UserUpdateScreen';
+
+const FurnitureStackNavigator = createStackNavigator({
+  Furniture: {
+    screen: FurnitureScreen,
+    navigationOptions: {
+      headerTitle: 'Furniture'
+    }
+  }
+}, {
+  initialRouteName: 'Furniture',
+  headerLayoutPreset: 'center'
 });
+
+const UserStackNavigator = createStackNavigator({
+  User: {
+    screen: UserScreen,
+    navigationOptions: {
+      headerTitle: 'User'
+    }
+  },
+  UserUpdate: {
+    screen: UserUpdateScreen,
+    navigationOptions: {
+      headerTitle: 'Update User'
+    }
+  },
+  UserAdd: {
+    screen: UserAddScreen,
+    navigationOptions: {
+      headerTitle: 'Add User'
+    }
+  }
+}, {
+  initialRouteName: 'User',
+  headerLayoutPreset: 'center'
+});
+
+const BottomTabNavigator = createMaterialTopTabNavigator({
+  FurnitureTab: {
+    screen: FurnitureStackNavigator,
+    navigationOptions: {
+      tabBarLabel: 'Furniture',
+      tabBarIcon: ({ tintColor }) => (
+        <IconFa
+          name='list'
+          size={16}
+          color={tintColor}
+        />
+      )
+    }
+  },
+  UserTab: {
+    screen: UserStackNavigator,
+    navigationOptions: {
+      tabBarLabel: 'User',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <IconFa
+          name={focused ? 'user' : 'user-o'}
+          size={16}
+          color={tintColor}
+        />
+      )
+    }
+  },
+}, {
+  initialRouteName: 'FurnitureTab',
+  tabBarPosition: 'bottom',
+  tabBarOptions: {
+    activeTintColor: 'black',
+    inactiveTintColor: 'grey',
+    showIcon: true,
+    labelStyle: {
+      fontSize: 12
+    },
+    indicatorStyle: {
+      height: 0
+    },
+    upperCaseLabel: false,
+    style: {
+      backgroundColor: 'white',
+      borderTopWidth: 1,
+      borderTopColor: '#ddd'
+    },
+    tabStyle: {
+      height: 56
+    },
+  }
+});
+
+const SwitchNavigator = createSwitchNavigator({
+  BottomTabNavigator
+});
+
+const AppContainer = createAppContainer(SwitchNavigator);
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
+        <AppContainer />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
